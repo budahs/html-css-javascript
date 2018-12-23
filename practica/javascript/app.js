53fe2e2b4c4c26f92b3dc097deb53433ddc30ff6
Pablo Garcia <budaya.anim@gmail.com>
2018-12-23 23:07:45 +0100

    Cambios urls archivos

--- practica/javascript/app.js
+++ practica/javascript/app.js
@@ -1,5 +1,5 @@
-import {setMenu} from '/practica/javascript/menu.js'
-//import {setForm} from '/practica/javascript/form.js' // ojo esto de incorporar funciones de forma modular solo funciona en ES6 Node usa @import todavia
+import {setMenu} from '/javascript/menu.js'
+//import {setForm} from '/javascript/form.js' // ojo esto de incorporar funciones de forma modular solo funciona en ES6 Node usa @import todavia
 function app(){
     setMenu()
     //setForm()
